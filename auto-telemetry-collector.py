b9842ae5ddeca1e09fe43542da66440685578091
Ramon Buckland <ramon@thebuckland.com>
2020-06-13 17:35:22 +1000

    wip: prometheus version

--- auto-telemetry-collector.py
+++ auto-telemetry-collector.py
@@ -23,12 +23,16 @@ sys.stdout.flush()
 from prometheus_client import start_http_server
 from prometheus_client import Gauge
 import obd
-obd.logger.setLevel(obd.logging.DEBUG)
+# obd.logger.setLevel(obd.logging.DEBUG)
 
 def connect_and_watch(device):
     connection = obd.Async(arguments["--device"]) # auto connect
+    print("####################################", flush=True)
+    for c in connection.supported_commands:
+        print(f"-  {c.name} on ECU {c.ecu}")
+    print("####################################")
     connection.watch(obd.commands.RPM, callback=collect_RPM)
-    connection.watch(obd.commands.FUEL_PRESSURE, callback=collect_FuelPressure)
+    connection.watch(obd.commands.FUEL_RAIL_PRESSURE_DIRECT, callback=collect_FUEL_RAIL_PRESSURE_DIRECT)
     connection.start() # start the async update loop
     print (". OBD Async Started")
     return connection
@@ -39,10 +43,10 @@ def collect_RPM(rpm):
     #print(f"RPM = [{rpm.value.magnitude}]")
     g_rpm.set(rpm.value.magnitude)   # Set to a given value
 
-def collect_FuelPressure(fuel_pressure):
+def collect_FUEL_RAIL_PRESSURE_DIRECT(x):
     #print(fuel_pressure)
     #print(f"RPM = [{fuel_pressure.value.magnitude}]")
-    g_fuel_pressure.set(fuel_pressure.value.magnitude)   # Set to a given value
+    g_fuel_pressure.set(x.value.magnitude)   # Set to a given value
 
 if __name__ == "__main__":
     arguments = docopt(__doc__, version='1.0.1')
@@ -55,10 +59,12 @@ if __name__ == "__main__":
     device = arguments["--device"]
 
     conn = connect_and_watch(device)
+
     while True:
-        status = conn.status()
-        print(f"{device} is {status}")
-        if status == "Not Connected":
+         status = conn.status()
+         print(f"{device} is {status}")
+         if status == "Not Connected":
             conn = connect_and_watch(device)
-        # print("{} is {}".format({arguments["--device"],1))
-        time.sleep(10)
+        
+         print("{} is {}".format(arguments["--device"], status))
+         time.sleep(10)
